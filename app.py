72f2f27358da1a85ea47ffec97b08c34f2024c18
Kazuo Xiang <55776732+Azure-Whale@users.noreply.github.com>
2021-02-09 13:11:05 -0500

    add some comments

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from enum import Enum 
+from enum import Enum
 import logging
 from random import choice
 
 logging.basicConfig(filename='Logs/game.log', level=logging.DEBUG)
 
-class TicTacToe: 
-    class STATES(Enum): 
-        CROSS_TURN = 0 
-        NAUGHT_TURN = 1 
-        DRAW = 2 
-        CROSS_WON = 3 
-        NAUGHT_WON = 4 
+
+class TicTacToe:
+    class STATES(Enum):
+        CROSS_TURN = 0
+        NAUGHT_TURN = 1
+        DRAW = 2
+        CROSS_WON = 3
+        NAUGHT_WON = 4
 
     def __init__(self):
         # Customize the game setting
         self.setting = {
-                'first': None, # the first player will be decided by its value
+            'first': None,  # the first player will be decided by its value
         }
         # init the game board, '_' marks the free place, 'o' marks the places hold by o, 'x' marks the places hold by x
         self.board = [['_' for row in range(3)] for col in range(3)]
@@ -23,49 +24,65 @@ class TicTacToe:
         self.current_statue = None
         self.remain_places = 9
         logging.info('Game Ready')
-    
-    def choose_game_mode(self,mode = None):
+
+    def choose_game_mode(self, symbol=None):
         """
         The func helps the player input who is the first according to given mode, otherwise it would be random
+
+        @symbol: the symbol who is going to play first
         """
-        if mode == 'o':
+        if symbol == 'o':
             self.setting['first'] = 'o'
             self.current_statue = self.STATES.NAUGHT_TURN
-        elif mode == 'x':
+        elif symbol == 'x':
             self.setting['first'] = 'x'
             self.current_statue = self.STATES.CROSS_TURN
         else:
-            self.setting['first'] = choice(['o','x'])
+            self.setting['first'] = choice(['o', 'x'])
             self.current_statue = self.choose_game_mode(self.setting['first'])
 
     def place_marker(self, symbol, row, column):
         """
         According to given sympol and coordinate, the func will try to place a marker to target place
+
+        @symbol: the symbol who is going to play first
+        @row: the index of row of targe place
+        @column: the index of column of targe place
         """
         # if the game is over, operation is invaild.
-        if self.current_statue in {self.STATES.DRAW,self.STATES.CROSS_WON,self.STATES.NAUGHT_WON}:
+        if self.current_statue in {self.STATES.DRAW, self.STATES.CROSS_WON, self.STATES.NAUGHT_WON}:
             logging.warning('Invaild operation, the game is over.')
             return
         # if target place is used, operation is invaild.
         if self.board[row][column] != '_':
-            logging.warning('Invaild operation, the place has been hold by {}'.format(self.board[row][column]))
+            logging.warning('Invaild operation, the place has been hold by {}'.format(
+                self.board[row][column]))
             return
         # if the player plays in correct turn, place a new marker in available target place and update game statue
-        if symbol == 'x' and self.current_statue==self.STATES.CROSS_TURN:
+        if symbol == 'x' and self.current_statue == self.STATES.CROSS_TURN:
             self.board[row][column] = 'x'
-            self.update_game('x',row,column)
-        elif symbol == 'o' and self.current_statue==self.STATES.NAUGHT_TURN:
+            self.update_game('x', row, column)
+        elif symbol == 'o' and self.current_statue == self.STATES.NAUGHT_TURN:
             self.board[row][column] = 'o'
-            self.update_game('o',row,column)
+            self.update_game('o', row, column)
         # if the player plays when it is not his/her turn, the operation is invaild
         else:
-            logging.warning('Invaild operation, the game is played in turns! Current player is {}'.format(self.current_statue))
+            logging.warning('Invaild operation, the game is played in turns! Current player is {}'.format(
+                self.current_statue))
             return
-    
-    def update_game(self,symbol,row,column):
+
+    def update_game(self, symbol, row, column):
+        """
+        Update the current game statue according to tic-tac-toe game rules
+
+        @symbol: the symbol who is going to play first
+        @row: the index of row of most recently placed marker
+        @column: the index of column of most recently placed marker
+
+        """
         # the available places minus 1 when a new marker placed
         self.remain_places -= 1
-        
+
         """Check conditions which may create a winner"""
         # check if the new marker forms a row having three same symbol
         def check_row():
@@ -73,8 +90,9 @@ class TicTacToe:
             for i in range(3):
                 if self.board[i][column] == symbol:
                     cnt += 1
-            return True if cnt ==3 else False
+            return True if cnt == 3 else False
         # check if the new marker forms a column having three same symbol
+
         def check_col():
             cnt = 0
             for i in range(3):
@@ -82,17 +100,18 @@ class TicTacToe:
                     cnt += 1
             return True if cnt == 3 else False
         # check if the new marker forms a diagonal having three same symbol
+
         def check_diagonal():
             cnt1 = cnt2 = 0
             row = list(range(3))
             col = list(range(3))
-            for i,j in zip(row,col):
+            for i, j in zip(row, col):
                 if self.board[i][j] == symbol:
                     cnt1 += 1
-            for i,j in zip(row,col[::-1]):
+            for i, j in zip(row, col[::-1]):
                 if self.board[i][j] == symbol:
                     cnt2 += 1
-            return True if max(cnt1,cnt2) == 3 else False
+            return True if max(cnt1, cnt2) == 3 else False
 
         """According to current board, update game statue"""
         # if there is no winner for now
@@ -118,7 +137,6 @@ class TicTacToe:
                 self.current_statue = self.STATES.CROSS_WON
                 logging.info('Game Over, naguht wins.. ')
         return self.current_statue
-        
 
     def print_board(self):
         """
--- test_game.py
+++ test_game.py
@@ -1,5 +1,6 @@
-#test_game.py
+# test_game.py
 from app import TicTacToe
+
 
 def test_col_win():
     """
@@ -7,13 +8,14 @@ def test_col_win():
     """
     game = TicTacToe()
     game.choose_game_mode('o')
-    game.place_marker('o',0,0)
-    game.place_marker('x',0,1)
-    game.place_marker('o',1,0)
-    game.place_marker('x',0,2)
-    game.place_marker('o',2,0)
+    game.place_marker('o', 0, 0)
+    game.place_marker('x', 0, 1)
+    game.place_marker('o', 1, 0)
+    game.place_marker('x', 0, 2)
+    game.place_marker('o', 2, 0)
     game.print_board()
     assert game.current_statue == game.STATES.NAUGHT_WON
+
 
 def test_row_win():
     """
@@ -21,13 +23,14 @@ def test_row_win():
     """
     game = TicTacToe()
     game.choose_game_mode('x')
-    game.place_marker('x',0,0)
-    game.place_marker('o',0,1)
-    game.place_marker('x',1,0)
-    game.place_marker('o',0,2)
-    game.place_marker('x',2,0)
+    game.place_marker('x', 0, 0)
+    game.place_marker('o', 0, 1)
+    game.place_marker('x', 1, 0)
+    game.place_marker('o', 0, 2)
+    game.place_marker('x', 2, 0)
     game.print_board()
     assert game.current_statue == game.STATES.CROSS_WON
+
 
 def test_diagrame_win():
     """
@@ -35,14 +38,15 @@ def test_diagrame_win():
     """
     game = TicTacToe()
     game.choose_game_mode('x')
-    game.place_marker('x',0,1)
-    game.place_marker('o',1,1)
-    game.place_marker('x',0,2)
-    game.place_marker('o',2,2)
-    game.place_marker('x',1,2)
-    game.place_marker('o',0,0)
+    game.place_marker('x', 0, 1)
+    game.place_marker('o', 1, 1)
+    game.place_marker('x', 0, 2)
+    game.place_marker('o', 2, 2)
+    game.place_marker('x', 1, 2)
+    game.place_marker('o', 0, 0)
     game.print_board()
     assert game.current_statue == game.STATES.NAUGHT_WON
+
 
 def test_draw():
     """
@@ -50,15 +54,15 @@ def test_draw():
     """
     game = TicTacToe()
     game.choose_game_mode('o')
-    game.place_marker('o',0,0)
-    game.place_marker('x',0,1)
-    game.place_marker('o',0,2)
-    game.place_marker('x',1,1)
-    game.place_marker('o',1,0)
-    game.place_marker('x',2,0)
-    game.place_marker('o',1,2)
-    game.place_marker('x',2,2)
-    game.place_marker('o',2,1)
+    game.place_marker('o', 0, 0)
+    game.place_marker('x', 0, 1)
+    game.place_marker('o', 0, 2)
+    game.place_marker('x', 1, 1)
+    game.place_marker('o', 1, 0)
+    game.place_marker('x', 2, 0)
+    game.place_marker('o', 1, 2)
+    game.place_marker('x', 2, 2)
+    game.place_marker('o', 2, 1)
     game.print_board()
     assert game.current_statue == game.STATES.DRAW
 
